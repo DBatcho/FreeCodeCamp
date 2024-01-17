898426a270477b70417e64b9cde4dd4608414de3
DBatcho <dbatch823@gmail.com>
2024-01-17 15:55:07 -0500

    moved check hour if 12 if statement until after hour while loop solves hour. Added if statements to handle the (next day) and (n days later) output by using count to determine how many days have passed. Saved under new variable dayslater Added if statement when combining all variables to include dayslater if it was used

--- Time_Calculator/time_calculator.py
+++ Time_Calculator/time_calculator.py
@@ -6,22 +6,27 @@ def add_time(start, duration):
     #d_hour
     #d_min
     count = 0
+    dayslater = None
 
     colonpos = start.find(':')
     hour = int(start[0:colonpos].strip())
     min = int(start[colonpos + 1:colonpos + 3].strip())
     meridiem = start[colonpos + 3:].strip()
+
     print ("Start Time:")
     print (hour)
     print (min)
     print (meridiem)
 
+
     colonpos = duration.find(':')
     d_hour = int(duration[0:colonpos].strip())
     d_min = int(duration[colonpos + 1:].strip())
+
     print ("Duration Time:")
     print (d_hour)
     print (d_min)
+ 
 
     if (meridiem == "PM"):
         count += 1
@@ -30,28 +35,40 @@ def add_time(start, duration):
     if (min >= 60):
         min -= 60
         hour += 1
-    
-    if (hour == 12):
-        count += 1
-    
+
     hour = hour + d_hour
+    print (hour)
     if (hour > 12):
         while hour > 12:
             hour -= 12
             count += 1
+    
+    if (hour == 12):
+        count += 1
 
     if (count % 2 == 0):
         meridiem = "AM"
     else:
         meridiem = "PM"
+    
+    if (count >= 4):
+        dayslater = "(" + str(count//2) + " days later)"
+    elif (count >= 2):
+        dayslater = "(next day)"
 
     min = str(min)
     if (len(min) < 2):
         min = "0" + min
+    
+    if (dayslater == None):
+        new_time = str(hour) + ":" + str(min) + " " + meridiem
+    else:
+        new_time = str(hour) + ":" + str(min) + " " + meridiem + " " + dayslater
 
-    new_time = str(hour) + ":" + str(min) + " " + meridiem
     return new_time
 
+"""
+#PASS
 actual = add_time("3:30 PM", "2:12")
 expected = "5:42 PM"
 if (actual == expected):
@@ -63,6 +80,7 @@ else:
     print (actual)
     print (expected)
 
+#PASS
 actual = add_time("11:55 AM", "3:12")
 expected = "3:07 PM"
 if (actual == expected):
@@ -74,6 +92,7 @@ else:
     print (actual)
     print (expected)
 
+#PASS
 actual = add_time("9:15 PM", "5:30")
 expected = "2:45 AM (next day)"
 if (actual == expected):
@@ -85,6 +104,7 @@ else:
     print (actual)
     print (expected)
 
+#PASS
 actual = add_time("11:40 AM", "0:25")
 expected = "12:05 PM"
 if (actual == expected):
@@ -96,6 +116,7 @@ else:
     print (actual)
     print (expected)
 
+#PASS
 actual = add_time("2:59 AM", "24:00")
 expected = "2:59 AM (next day)"
 if (actual == expected):
@@ -107,6 +128,7 @@ else:
     print (actual)
     print (expected)
 
+#PASS
 actual = add_time("11:59 PM", "24:05")
 expected = "12:04 AM (2 days later)"
 if (actual == expected):
@@ -118,6 +140,7 @@ else:
     print (actual)
     print (expected)
 
+#PASS
 actual = add_time("8:16 PM", "466:02")
 expected = "6:18 AM (20 days later)"
 if (actual == expected):
@@ -129,6 +152,7 @@ else:
     print (actual)
     print (expected)
 
+#PASS
 actual = add_time("5:01 AM", "0:00")
 expected = "5:01 AM"
 if (actual == expected):
@@ -139,6 +163,7 @@ else:
     print ("FAIL")
     print (actual)
     print (expected)
+"""
 
 actual = add_time("3:30 PM", "2:12", "Monday")
 expected = "5:42 PM, Monday"
